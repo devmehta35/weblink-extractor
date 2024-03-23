e6b9d9b8e9a187370e8d71d91621ebd0bfa7193a
Divyansh <158802839+devmehta35@users.noreply.github.com>
2024-03-23 13:45:14 +0000

    updated...

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import time
+from googlesearch import search
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Hello, World!'
+    if request.method == 'POST':
+        query = request.form.get('query')
+        start_time = time.monotonic()
+        urls = [url for url in search(query)]
+        end_time = time.monotonic()
+        execution_time = end_time - start_time
+        return render_template('results.html', urls=urls, execution_time=execution_time)
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
