36854de66d3a291f95d43d57fc5a24302e74958e
Divyansh <158802839+devmehta35@users.noreply.github.com>
2024-03-25 11:34:44 +0000

    MongoDB based funtionality added...

--- app.py
+++ app.py
@@ -3,25 +3,41 @@ from threading import Thread
 import time
 import uuid
 from googlesearch import search
+from pymongo import MongoClient
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Setup MongoDB Atlas connection
+uri = os.getenv('MONGODB_URI')
+client = MongoClient(uri)
+db = client['user_data']
+collection = db['searches']
 
 app = Flask(__name__)
 results = {}
 
-def background_search(query, id):
+def background_search(query, id, ip_address):
     print(f"Starting search for query: {query}")
     start_time = time.monotonic()
     urls = [url for url in search(query)]
     print(f"Search completed. Found {len(urls)} results.")
     end_time = time.monotonic()
-    execution_time = end_time - start_time
+    execution_time = round(end_time - start_time, 2)  # Round off to 2 decimal places
     results[id] = (urls, execution_time)
+    # Store the data in MongoDB
+    collection.insert_one({'ip': ip_address, 'query': query, 'num_results': len(urls), 'uuid': id, 'execution_time': execution_time, 'timestamp': datetime.now()})
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         query = request.form.get('query')
         id = str(uuid.uuid4())
-        Thread(target=background_search, args=(query, id)).start()
+        ip_address = request.remote_addr
+        Thread(target=background_search, args=(query, id, ip_address)).start()
         return render_template('loading.html', id=id)
     return render_template('index.html')
 
@@ -31,3 +47,6 @@ def results_page(id):
         urls, execution_time = results[id]
         return render_template('results.html', urls=urls, execution_time=execution_time, num_results=len(urls))
     return "Results not ready, please refresh the page."
+
+if __name__ == '__main__':
+    app.run(debug=True)
